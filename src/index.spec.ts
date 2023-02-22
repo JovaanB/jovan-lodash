367c731c0575c1ffbf6b17a2d632bb23cdcce206
Jovan <bienvenujovan@gmail.com>
2023-02-22 01:09:44 +0100

    Write first loash function in TS -> isBoolean

---
+++ src/index.spec.ts
@@ -0,0 +1,19 @@
+import { isBoolean } from "./isBoolean";
+
+describe("isBoolean", () => {
+  test("should return false for isBoolean(6)", () => {
+    expect(isBoolean(6)).toBe(false);
+  });
+
+  test("should return true for isBoolean(true)", () => {
+    expect(isBoolean(true)).toBe(true);
+  });
+
+  test("should return true for isBoolean(false)", () => {
+    expect(isBoolean(false)).toBe(true);
+  });
+
+  test("should return false for isBoolean(1)", () => {
+    expect(isBoolean("1")).toBe(false);
+  });
+});
