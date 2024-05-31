96a08ee6b86e311bfc4d92b71a049122b34fd316
JovaanB <bienvenujovan@gmail.com>
2024-06-01 00:32:18 +0200

    feat: use vitest & fix isBoolean unit tests

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,27 +1,64 @@
 import { isBoolean } from "./index";
+import { describe, test, expect } from "vitest";
 
 describe("isBoolean", () => {
-  test("should return false for isBoolean(6)", () => {
+  test("should return false for 6", () => {
     expect(isBoolean(6)).toBe(false);
   });
 
-  test("should return true for isBoolean(true)", () => {
+  test("should return true for true", () => {
     expect(isBoolean(true)).toBe(true);
   });
 
-  test("should return true for isBoolean(false)", () => {
+  test("should return true for false", () => {
     expect(isBoolean(false)).toBe(true);
   });
 
-  test("should return false for isBoolean(1)", () => {
+  test("Should return true for Boolean(true)", () => {
+    expect(isBoolean(Boolean(true))).toBe(true);
+  });
+
+  test("Should return true for Boolean(false)", () => {
+    expect(isBoolean(Boolean(false))).toBe(true);
+  });
+
+  test("should return false for 1", () => {
     expect(isBoolean("1")).toBe(false);
   });
 
-  test("should return false for isBoolean(1)", () => {
+  test("should return false for !!0", () => {
     expect(isBoolean(!!0)).toBe(true);
   });
 
-  test("should return false for isBoolean(1)", () => {
+  test("should return false for 0", () => {
     expect(isBoolean(Boolean(0))).toBe(true);
+  });
+
+  test("should return false for null", () => {
+    expect(isBoolean(null)).toBe(false);
+  });
+
+  test("should return false for undefined", () => {
+    expect(isBoolean(undefined)).toBe(false);
+  });
+
+  test("should return false for NaN", () => {
+    expect(isBoolean(NaN)).toBe(false);
+  });
+
+  test("should return false for Infinity", () => {
+    expect(isBoolean(Infinity)).toBe(false);
+  });
+
+  test("should return false for -Infinity", () => {
+    expect(isBoolean(-Infinity)).toBe(false);
+  });
+
+  test("should return false for 'true'", () => {
+    expect(isBoolean("true")).toBe(false);
+  });
+
+  test("should return false for 'false'", () => {
+    expect(isBoolean("false")).toBe(false);
   });
 });
